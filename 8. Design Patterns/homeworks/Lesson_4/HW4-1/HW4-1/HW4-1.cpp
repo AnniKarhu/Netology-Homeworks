f9095181bd6dcf1088d67ba6b95c43b1aeed59dd
Annika <anna_karhu@mail.ru>
2024-04-11 23:17:41 +0300

    homework 8-4

---
+++ 8. Design Patterns/homeworks/Lesson_4/HW4-1/HW4-1/HW4-1.cpp
@@ -0,0 +1,95 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0;
+};
+
+class Data : public Printable
+{
+public:
+    enum class Format
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    Data(std::string data, Format format)
+        : data_(std::move(data)), format_(format) {}
+
+    std::string printAsHTML() const override
+    {
+        if (format_ != Format::kHTML) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string printAsText() const override
+    {
+        if (format_ != Format::kText) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return data_;
+    }
+    std::string printAsJSON() const override
+    {
+        if (format_ != Format::kJSON) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+
+private:
+    std::string data_;
+    Format format_;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable, Data::Format format)
+{
+    switch (format)
+    {
+    case Data::Format::kText:
+        file << printable.printAsText();
+        break;
+    case Data::Format::kJSON:
+        file << printable.printAsJSON();
+        break;
+    case Data::Format::kHTML:
+        file << printable.printAsHTML();
+        break;
+    }
+}
+
+void saveToAsHTML(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kHTML);
+}
+
+void saveToAsJSON(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kJSON);
+}
+
+void saveToAsText(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kText);
+}
+
+int main()
+{
+    return 0;
+}
+
+//Данный код нарушает принцип открытости/закрытости: функция SaveTo - потребуется изменение уже имеющегося кода функции в случае, если нужно будет добавить новые типы данных. 
+// Кроме того, изменения придется вносить в разные части кода - и в класс Data, и в функцию SaveTo.
+//Данный код нарушает принцип подстановки Барбары Лисков - существует вероятность, что созданный объект вызовет метод, который вызовет исключение, хотя в ином случае 
+// (другой экземпляр объекта) отработает корректно. Здесь экземпляры наследников не всегда могут использовать метод базового класса. Data является наследником printable, но не допускает выполнение методов в некоторых случаях.
+//Нарушенмпринцип разделения интерфесов - "толстый интерфейс" у базового класса - узконаправленные методы в базовом классе printable - virtual std::string printAsHTML() const = 0; , virtual std::string printAsText() const = 0; ,
+// virtual std::string printAsJSON() const = 0;
+
+
+//В  данном случае стоит создать наследников от класса Data, избавиться от узкоспециализированных методов в базовом классе, от switch-case, не допускать вызова методов для классов-наследников, если эти методы к ним неприменимы, 
+// чтобы вместо выбрасывания исключений не допускать компиляцию такого кода.
---
+++ 8. Design Patterns/homeworks/Lesson_4/HW4-2/HW4-2/HW4-2.cpp
@@ -0,0 +1,189 @@
+﻿#include <fstream>
+
+enum class Format
+{
+    kText,
+    kHTML,
+    kJSON
+};
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    
+    virtual std::string print() const = 0;
+    virtual std::string get_plain_text() const = 0;
+    virtual Format get_format() const = 0;
+   
+protected:
+    Printable(std::string data) : data_(std::move(data)) {};
+    std::string data_;
+
+};
+
+class Data_HTML : public Printable
+{
+public:
+    Data_HTML(std::string data)  : Printable(data) {}
+    std::string print() const override
+    {
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string get_plain_text() const override
+    {
+        return  data_ ;
+    };
+    Format get_format() const {
+        return Format::kHTML;  
+    }
+};
+
+class Data_Text : public Printable
+{
+public:
+    Data_Text(std::string data) : Printable(data) {}
+    std::string print() const override
+    {
+        return data_;
+    }
+    std::string get_plain_text() const override
+    {
+        return  data_;
+    };
+    Format get_format() const {
+        return Format::kText;  
+    }
+};
+
+class Data_JSON : public Printable
+{
+public:
+    Data_JSON(std::string data) : Printable(data) {}
+    std::string print() const override
+    {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+    std::string get_plain_text() const override
+    {
+        return  data_;
+    };
+    Format get_format() const {
+        return Format::kJSON;  
+    }
+};
+
+
+void saveTo(std::ofstream& file, const Printable& printable)
+{
+     file << printable.print();       
+}
+
+
+void saveToAsHTML(std::ofstream& file, const Printable& printable) {
+    Data_HTML data_html_obj(printable.get_plain_text());
+    saveTo(file, data_html_obj);
+}
+
+void saveToAsJSON(std::ofstream& file, const Printable& printable) {
+        
+    Data_JSON data_json_obj(printable.get_plain_text());
+    saveTo(file, data_json_obj);
+}
+
+void saveToAsText(std::ofstream& file, const Printable& printable) {
+    Data_Text data_text_obj(printable.get_plain_text());
+    saveTo(file, data_text_obj);   
+}
+
+void saveToAsHTML_2(std::ofstream& file, const Printable& printable) {
+
+    if (printable.get_format() != Format::kHTML)
+    {
+        throw std::runtime_error("Invalid format!");
+    }
+    Data_HTML data_html_obj(printable.get_plain_text());
+    saveTo(file, data_html_obj);
+}
+
+void saveToAsJSON_2(std::ofstream& file, const Printable& printable) {
+
+    if (printable.get_format() != Format::kJSON)
+    {
+        throw std::runtime_error("Invalid format!");
+    }
+    Data_JSON data_json_obj(printable.get_plain_text());
+    saveTo(file, data_json_obj);
+}
+
+void saveToAsText_2(std::ofstream& file, const Printable& printable) {
+    if (printable.get_format() != Format::kText)
+    {
+        throw std::runtime_error("Invalid format!");
+    }
+    Data_Text data_text_obj(printable.get_plain_text());
+    saveTo(file, data_text_obj);
+}
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Data_HTML data_html_obj("some data for HTML object");
+    std::cout << data_html_obj.print() << "\n";
+
+    Data_JSON data_json_obj("some data for JSON object");
+    std::cout << data_json_obj.print() << "\n";
+
+    Data_Text data_text_obj("some data for text object");
+    std::cout << data_text_obj.print() << "\n";
+
+    std::ofstream file_("sample_file.txt", std::ios::app);
+    if (file_.is_open())
+    {
+        saveTo(file_, data_html_obj);
+        file_ <<  "\n"; 
+        saveToAsHTML(file_, data_html_obj);
+        file_ << "\n";
+        saveToAsJSON(file_, data_html_obj);
+        file_ << "\n";
+        saveToAsText(file_, data_html_obj);
+        file_ << "\n";
+        file_ << "*********************\n\n";
+
+
+        saveTo(file_, data_json_obj);
+        file_ << "\n";
+        saveToAsHTML(file_, data_json_obj);
+        file_ << "\n";
+        saveToAsJSON(file_, data_json_obj);
+        file_ << "\n";
+        saveToAsText(file_, data_json_obj);
+        file_ << "\n";
+        file_ << "*********************\n\n";
+
+        saveTo(file_, data_text_obj);
+        file_ << "\n";
+        saveToAsHTML(file_, data_text_obj);
+        file_ << "\n";
+        saveToAsJSON(file_, data_text_obj);
+        file_ << "\n";
+        saveToAsText(file_, data_text_obj);
+        file_ << "\n";
+        file_ << "*********************\n\n";
+
+        try
+        {
+            saveToAsHTML_2(file_, data_json_obj);
+        }
+        catch (const std::exception& ex)
+        {
+            std::cout << ex.what() << "\n";
+        }
+    }
+    file_.close();
+
+}
+
